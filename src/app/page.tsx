4f0738fb57ed70e5b9698654055826b9adf90d6c
Gaurav Shukla <shukla.gaurav@rwit.io>
2026-02-18 15:20:23 +0530

    Learn relative colors syntax in CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,13 @@ export default function Home() {
   return (
     <div>
       <h1>Practiced concepts</h1>
-      
+
       <ol>
         <li>
           <Link href="/at-property">The @property Rule</Link>
+        </li>
+        <li>
+          <Link href="/relative-colors">Relative Colors</Link>
         </li>
       </ol>
     </div>
---
+++ src/app/relative-colors/page.tsx
@@ -0,0 +1,22 @@
+import "./page.css";
+
+export default function Page() {
+  return (
+    <div className="container">
+      <div className="colors-group-1">
+        <div className="box box-1"></div>
+        <div className="box box-2"></div>
+      </div>
+
+      <div className="colors-group-2">
+        <div className="box box-3"></div>
+        <div className="box box-4"></div>
+      </div>
+
+      <div className="gradients">
+        <div className="gradient gradient-1"></div>
+        <div className="gradient gradient-2"></div>
+      </div>
+    </div>
+  );
+}
